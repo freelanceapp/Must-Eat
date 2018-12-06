c22b2a145ebd1d235c7bb0c212e4b7aa70892de3
amar <amardeep.infobite@gmail.com>
2018-12-06 16:08:37 +0530

    permission and location

--- app/src/main/java/infobite/must/eat/ui/activities/FindLocationActivity.java
+++ app/src/main/java/infobite/must/eat/ui/activities/FindLocationActivity.java
@@ -1,34 +1,199 @@
-
 package infobite.must.eat.ui.activities;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import infobite.must.eat.R;
+import infobite.must.eat.utils.LocationTrack;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
-public class FindLocationActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView manual_location_btn;
+public class FindLocationActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
+    TextView manual_location_btn, currnet_location_btn;
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 123;
+    private String provider;
+    LocationTrack locationTrack;
+    Double lat, longi;
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_location);
-        manual_location_btn = (TextView)findViewById(R.id.manual_location_btn);
-        manual_location_btn.setOnClickListener(this);
 
+        //get the permissions we have asked for before but are not granted..
+        //we will store this in a global list to access later.
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        manual_location_btn = (TextView) findViewById(R.id.manual_location_btn);
+        manual_location_btn.setOnClickListener(this);
+        currnet_location_btn = (TextView) findViewById(R.id.currnet_location_btn);
+        currnet_location_btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.manual_location_btn :
-                Intent intent = new Intent(FindLocationActivity.this,ManualLocationActivity.class);
+        switch (view.getId()) {
+            case R.id.manual_location_btn:
+                Intent intent = new Intent(FindLocationActivity.this, ManualLocationActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.currnet_location_btn:
+                getLocation();
+
+                break;
         }
+    }
+
+
+
+    protected void checkPermission(){
+        if( ContextCompat.checkSelfPermission
+                (FindLocationActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            // Do something, when permissions not granted
+            if( ActivityCompat.shouldShowRequestPermissionRationale(
+                    FindLocationActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)){
+                // If we should give explanation of requested permissions
+                // Show an alert dialog here with request explanation
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(FindLocationActivity.this);
+                builder.setMessage("Location " + " Storage permissions are required to do the task.");
+                builder.setTitle("Please grant those permissions");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(
+                                FindLocationActivity.this,
+                                new String[]{ Manifest.permission.ACCESS_FINE_LOCATION
+                                },
+                                MY_PERMISSIONS_REQUEST_CODE
+                        );
+                    }
+                });
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                android.support.v7.app.AlertDialog dialog = builder.create();
+                dialog.show();
+            }else{
+                // Directly request for required permissions, without explanation
+                ActivityCompat.requestPermissions(
+                        FindLocationActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                        },
+                        MY_PERMISSIONS_REQUEST_CODE
+                );
+            }
+        }else {
+            // Do something, when permissions are already granted
+            Toast.makeText(FindLocationActivity.this,"Permissions already granted",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CODE:{
+                // When request is cancelled, the results array are empty
+                if(
+                        (grantResults.length >0) &&
+                                (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        ){
+                    // Permissions are granted
+                    Toast.makeText(FindLocationActivity.this,"Permissions granted.",Toast.LENGTH_SHORT).show();
+
+                    // close this activity
+                    finish();
+                }else {
+                    // Permissions are denied
+                    Toast.makeText(FindLocationActivity.this,"Permissions denied.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, (LocationListener) this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(FindLocationActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            Toast.makeText(FindLocationActivity.this, "Location "+addresses.get(0).getAddressLine(0)+", "+
+                    addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2)+ location.getLatitude() +" "+ location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e)
+        {
+
+        }
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(FindLocationActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
     }
 }
--- app/src/main/java/infobite/must/eat/ui/activities/SplashScreenActivity.java
+++ app/src/main/java/infobite/must/eat/ui/activities/SplashScreenActivity.java
@@ -1,16 +1,26 @@
 package infobite.must.eat.ui.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import infobite.must.eat.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    public static String MY_PREFS_NAME = "SRIL_APP";
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 123;
     ImageView logoimage;
     TextView title;
 
@@ -22,13 +32,124 @@ public class SplashScreenActivity extends AppCompatActivity {
         logoimage = (ImageView) findViewById(R.id.logoimage);
         title = (TextView) findViewById(R.id.title);
 
-        new Handler().postDelayed(new Runnable() {
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkPermission();
+        }
+      /*  new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 2000);*/
     }
+
+    protected void checkPermission(){
+        if(ContextCompat.checkSelfPermission(SplashScreenActivity.this,Manifest.permission.CAMERA)
+                + ContextCompat.checkSelfPermission
+                (SplashScreenActivity.this,Manifest.permission.READ_CONTACTS)
+                + ContextCompat.checkSelfPermission
+                (SplashScreenActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                + ContextCompat.checkSelfPermission
+                (SplashScreenActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            // Do something, when permissions not granted
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    SplashScreenActivity.this,Manifest.permission.CAMERA)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    SplashScreenActivity.this,Manifest.permission.READ_CONTACTS)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    SplashScreenActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                    SplashScreenActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)){
+                // If we should give explanation of requested permissions
+                // Show an alert dialog here with request explanation
+                AlertDialog.Builder builder = new AlertDialog.Builder(SplashScreenActivity.this);
+                builder.setMessage("Camera, Read Contacts, Location and Write External" +
+                        " Storage permissions are required to do the task.");
+                builder.setTitle("Please grant those permissions");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(
+                                SplashScreenActivity.this,
+                                new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_CONTACTS,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION
+                                },
+                                MY_PERMISSIONS_REQUEST_CODE
+                        );
+                    }
+                });
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }else{
+                // Directly request for required permissions, without explanation
+                ActivityCompat.requestPermissions(
+                        SplashScreenActivity.this,
+                        new String[]{
+                                Manifest.permission.CAMERA,
+                                Manifest.permission.READ_CONTACTS,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        },
+                        MY_PERMISSIONS_REQUEST_CODE
+                );
+            }
+        }else {
+            // Do something, when permissions are already granted
+            Toast.makeText(SplashScreenActivity.this,"Permissions already granted",Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // This method will be executed once the timer is over
+                    // Start your app main activity
+                    Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                    // close this activity
+                    finish();
+                }
+            }, 3000);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CODE:{
+                // When request is cancelled, the results array are empty
+                if(
+                        (grantResults.length >0) &&
+                                (grantResults[0]
+                                        + grantResults[1]
+                                        + grantResults[2]
+                                        + grantResults[3]
+                                        == PackageManager.PERMISSION_GRANTED
+                                )
+                        ){
+                    // Permissions are granted
+                    Toast.makeText(SplashScreenActivity.this,"Permissions granted.",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(i);
+                    // close this activity
+                    finish();
+                }else {
+                    // Permissions are denied
+                    Toast.makeText(SplashScreenActivity.this,"Permissions denied.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+    
+    
 }
